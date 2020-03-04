f381dfcf659f6bfdc2431001b0ef6cf7a6bc77a3
Max Dayton <daytonmaximus@gmail.com>
2020-03-04 11:22:34 -0700

    nodemailer

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'daytonmaximus@gmail.com',
+    pass: 'pyth_JS-2.12'
+  }
+});
+
+sendEmail = () => {
+    var mailOptions = {
+      from: `${document.querySelector('#contact-email').value}@gmail.com`,
+      to: 'daytonmaximus@gmail.com',
+      subject: `Sent by ${document.querySelector('#contact-name').value} From wiserstorage.com!`,
+      text: `${document.querySelector('#contact-message').value}`
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    }); 
+}
